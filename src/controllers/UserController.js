98da668054c185b6cde13ad416c4f5e95ccd564a
Gabriel_Cunha <gabrielvlog11@gmail.com>
2023-07-11 11:08:28 -0300

    feat: "add controller to create user"

---
+++ src/controllers/UserController.js
@@ -0,0 +1,34 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default {
+  async createUser(req, res) {
+    try {
+      const { name, email } = req.body;
+  
+      // verificação pra saber se já existe um usuário com o e-mail
+      let user = await prisma.user.findUnique({
+        where: { email }
+      })
+  
+      // verificando se um usuário existe com o e-mail cadastrado
+      if(user) {
+        return res.json({ error: 'Já existe um usuário com esse e-mail' })
+      }
+  
+      user = await prisma.user.create({
+        data: {
+          name,
+          email,
+        },
+      });
+  
+      return res.json(user);
+    } catch (error) {
+      return res.json({
+         error 
+      })
+    }
+  }
+}
