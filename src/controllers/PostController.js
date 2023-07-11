a3449fed5567dd5856b398b29d69f82d1ea44b56
Gabriel_Cunha <gabrielvlog11@gmail.com>
2023-07-11 11:48:12 -0300

    feat: "creation of post routes and post creation method"

---
+++ src/controllers/PostController.js
@@ -0,0 +1,33 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+export default {
+  async createPost(req, res) {
+    const { content } = req.body;
+    const { id } = req.param;
+
+    try {
+      const user = await prisma.user.findUnique({
+        where: { id: Number(id) },
+      });
+
+      if (!user) {
+        return res.json({ message: "Usuário inexistente" });
+      }
+
+      const post = prisma.post.create({
+        data: {
+          content,
+          userID: user.id,
+        },
+        include: {
+          // traz as informações dos usuários junto ao post criado
+          author: true,
+        },
+      });
+      return res.json(post);
+    } catch (error) {
+      res.json({ message: error.message });
+    }
+  },
+};
